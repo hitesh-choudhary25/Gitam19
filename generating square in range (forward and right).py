4f6156026cf486c0788c7ef07325b5e83d9d5a84
hitesh-choudhary25 <51998309+hitesh-choudhary25@users.noreply.github.com>
2019-06-29 14:51:42 +0530

    Add files via upload

---
+++ generating square in range (forward and right).py
@@ -0,0 +1,8 @@
+import turtle as tt
+a1 = tt.Turtle()
+
+for x in range(4):
+  a1.forward(150)
+  a1.right(90)
+  
+tt.done()  
---
+++ generating square in range(backward and left).py
@@ -0,0 +1,8 @@
+import turtle as tt
+a1 = tt.Turtle()
+
+for x in range(4):
+  a1.backward(150)
+  a1.left(90)
+  
+tt.done()  
